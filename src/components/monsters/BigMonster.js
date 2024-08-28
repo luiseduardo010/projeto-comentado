b43cc4075bf28e31b4481844f06ceaf89d85ac21
Luis Eduardo <119073361+luiseduardo010@users.noreply.github.com>
2024-08-28 16:24:11 -0300

    Add files via upload

---
+++ src/components/monsters/BigMonster.js
@@ -0,0 +1,9 @@
+class BigMonster extends Character {
+    constructor() {
+        super('Big Monster')
+        this.life = 100
+        this.attack = 10
+        this.defense = 8
+        this.maxLife = this.life
+    }
+}
---
+++ src/components/monsters/LittleMonster.js
@@ -0,0 +1,9 @@
+class LittleMonster extends Character {
+    constructor() {
+        super('Little Monster')
+        this.life = 100
+        this.attack = 10
+        this.defense = 8
+        this.maxLife = this.life
+    }
+}
