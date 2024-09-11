b4b90dd9173037adf64a43c3ccfc94acdb3258eb
Luis Eduardo <119073361+luiseduardo010@users.noreply.github.com>
2024-09-11 08:22:42 -0300

    Projeto editado e finalizado!

---
+++ src/components/scenario/Log.js
@@ -0,0 +1,18 @@
+class Log {
+    list =  []
+    constructor(listElement) {
+        this.listElement = listElement
+    }
+
+    addMessage(msg) {
+        this.list.push(msg)
+        this.render()
+    }
+
+    render() {
+        this.listElement.innerHTML = ''
+        for (let i in this.list) {
+            this.listElement.innerHTML += `<li>${this.list[i]}</li>`
+        }
+    }
+}
